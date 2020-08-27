9bf31150f7a0357933e012cc97b37613a16a9025
Lucas-Garavaglia <lucasgrafimar@gmail.com>
2020-08-27 18:46:11 -0300

    Alteração para upar no servidor

--- bot.js
+++ bot.js
@@ -6,14 +6,30 @@ client.on("ready", () => {
   console.log(`Bot foi iniciado no servidor.`);
 });
 
-client.on("message", (msg) => {
-  if (msg.author.bot) return;
-  if (msg.channel.type === "dm") return;
-  const args = msg.content.trim().split(" ");
-  const command = args.shift();
-  if (command === `${config.prefix}ping`) {
-    msg.channel.send("Pong!");
+client.on("message", (message) => {
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+  if (message.content("!kick")) {
+    const user = message.mentions.users.first();
+    if (user) {
+      const member = message.guild.member(user);
+      if (member) {
+        member
+          .kick("Optional reason that will display in the audit logs")
+          .then(() => {
+            message.reply(`Successfully kicked ${user.tag}`);
+          })
+          .catch((err) => {
+            message.reply("I was unable to kick the member");
+            console.error(err);
+          });
+      } else {
+        message.reply("That user isn't in this guild!");
+      }
+    } else {
+      message.reply("You didn't mention the user to kick!");
+    }
   }
 });
 
-client.login(config.token);
+client.login(process.env.TOKEN);
