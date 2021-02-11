1a34ea95f6f7ddc12095a938c8aba3ce16b261d1
lucas-garavaglia <lucasgrafimar@gmail.com>
2021-02-11 09:32:43 -0300

    remoção de arquivos inuteis

--- command/clear.js
+++
@@ -1,7 +0,0 @@
-export default function clear(message) {
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-    return message.reply("Você não tem permissão para usar este comando.");
-  }
-  message.channel.bulkDelete(100);
-  message.channel.bulkDelete(100);
-}
